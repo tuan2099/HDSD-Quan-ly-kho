68b188872c04c1eb2f3d2e0ea40df0f60a4aaa65
tuan2099 <gwoqkepspsld@gmail.com>
2024-01-25 09:42:07 +0700

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Hướng dẫn sử dụng quản lý biển bảng PMC</span>,
+  project: {
+    link: "",
+  },
+  chat: {
+    link: "",
+  },
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  footer: {
+    text: "© Copyright 2023 by PMC",
+  },
+};
+
+export default config;
